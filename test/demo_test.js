eef879b37d89d69c078811b6cf50f650dea40306
aabellanosa <auabellanosa@hotmail.com>
2020-08-05 04:53:37 +0800

    testing saving a record in db, using promise and done methods

--- test/demo_test.js
+++ test/demo_test.js
@@ -2,6 +2,7 @@ const assert = require("assert");
 
 //Describe tests
 describe("Some demo tests", function () {
+  //one test
   it("adds two numbers", function () {
     assert(2 + 3 === 5);
   });
---
+++ test/saving_test.js
@@ -0,0 +1,16 @@
+const assert = require("assert");
+const MarioChar = require("../models/mariochar");
+
+//Describe test
+describe("Saving records", function () {
+  //a test
+  it("Saving a record into the database", function (done) {
+    var char = new MarioChar({
+      name: "Mario",
+    });
+    char.save().then(function () {
+      assert(char.isNew === false);
+      done();
+    });
+  });
+});
