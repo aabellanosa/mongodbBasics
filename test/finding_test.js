7605b381f7ab681122c527a090a86c50d6d4a406
aabellanosa <auabellanosa@hotmail.com>
2020-08-06 05:33:18 +0800

    added finding by ID (which is an object)

--- test/finding_test.js
+++ test/finding_test.js
@@ -1,10 +1,13 @@
 const assert = require("assert");
 const MarioChar = require("../models/mariochar");
 
+//making char global so we could use it to find by id
+var char;
+
 //Describe test
 describe("Finding records", function () {
   beforeEach(function (done) {
-    var char = new MarioChar({
+    char = new MarioChar({
       name: "Mario1",
     });
     char.save().then(function () {
@@ -17,6 +20,14 @@ describe("Finding records", function () {
   it("Finds a record from the database", function (done) {
     MarioChar.findOne({ name: "Mario1" }).then(function (result) {
       assert(result.name === "Mario1");
+      done();
+    });
+  });
+
+  //finding by id (which is an object)
+  it("Finds a record by id from the database", function (done) {
+    MarioChar.findOne({ _id: char._id }).then(function (result) {
+      assert(result._id.toString() === char._id.toString());
       done();
     });
   });
