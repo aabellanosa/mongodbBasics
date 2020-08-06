26155ca752171a92b4b73e45db14224715c929c3
aabellanosa <auabellanosa@hotmail.com>
2020-08-06 20:38:27 +0800

    update operators ($inc) for all weight property of the document

--- test/updating_test.js
+++ test/updating_test.js
@@ -9,10 +9,19 @@ describe("Updating records", function () {
   beforeEach(function (done) {
     char = new MarioChar({
       name: "Mario1",
+      weight: 50,
     });
     char.save().then(function () {
       done();
     });
+
+    // char2 = new MarioChar({
+    //   name: "Tawing",
+    //   weight: 68,
+    // });
+    // char2.save().then(function () {
+    //   done();
+    // });
   });
 
   //a test
@@ -25,5 +34,15 @@ describe("Updating records", function () {
         });
       }
     );
+  });
+
+  //a test
+  it("Increasing all weights by 1", function (done) {
+    MarioChar.updateMany({}, { $inc: { weight: 1 } }).then(function () {
+      MarioChar.findOne({ name: "Mario1" }).then(function (record) {
+        assert(record.weight === 51);
+        done();
+      });
+    });
   });
 });
